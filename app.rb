9d29ae0250eb431dcd7987a47546e71a28d6df01
LeraNickolaeva <leranick29@gmail.com>
2017-12-19 01:05:26 +0300

    Add auyh logic

--- app.rb
+++ app.rb
@@ -10,6 +10,20 @@ require './lib/quadratic_equation'
 
 set :public_folder, 'public'
 
+# Logic for protecting only certain URLs in the application
+# helpers do
+#   def protected!
+#     return if authorized?
+#     headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+#     halt 401, "Not authorized"
+#   end
+
+#   def authorized?
+#     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+#     @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+#   end
+# end
+
 get '/' do
   haml :index
 end
